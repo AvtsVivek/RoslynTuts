e406491bda89ce7a25cb0f0820a854380c3e72aa
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-04-16 00:08:24 +0530

    Added extension methids

--- src/Ex30001032/ReadMe.md
+++ src/Ex30001032/ReadMe.md
@@ -5,3 +5,15 @@ References:
 https://www.youtube.com/watch?v=J_Y1obNh_RA
 
 https://github.com/Flash0ver/F0-Talks-SourceGenerators
+
+See this in [https://sharplab.io/]
+
+using System;
+public class C {
+    public void Caller()
+    {
+        M(1,2,3);
+    }
+    public void M(params Object[] array) {
+    }
+}
--- src/Ex30001032/SourceGenTalks/Program.cs
+++ src/Ex30001032/SourceGenTalks/Program.cs
@@ -5,13 +5,39 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 
-
 //// namespace SourceGenTalks;
 
 ILogger logger = Program.CreateLogger(LogLevel.Trace);
 
-logger.LogInformation("Hello name!" );
+logger.LogInformation("Hello {name}!", "World" );
 
+await Task.Yield();
+
+logger.LogInformation("Hello {name}!", 0x_F0); // This is Hello 240!
+
+logger.Hello(9);
+
+logger.HelloWithSourceGenerator(9);
+// The above has performance hits.
+
+// So we need to use logger message extension.
+
+public static partial class LogExtensions
+{
+    private static Action<ILogger, int, Exception> hello = LoggerMessage.Define<int>(LogLevel.Information, 0x_F0, "Hello, {name}");
+    public static void Hello(this ILogger logger, int number)
+    {
+        hello.Invoke(logger, number, null!);
+    }
+
+    // The following is using source generators.
+    // To see where it has got the gnerated code,
+    // Right click HelloWithSourceGenerator and then go to definition.
+
+    [LoggerMessage(240, LogLevel.Information, "Hello, {name}!")]
+    public static partial void HelloWithSourceGenerator(this ILogger logger, int name);
+
+}
 
 
 //logger.Hello("World");
